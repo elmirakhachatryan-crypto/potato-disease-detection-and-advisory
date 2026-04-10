b9c818d5e562d8acabab2d406ddba659b387f283
elmirakhachatryan-crypto <elmirakhachatryan@gmail.com>
2026-04-11 00:04:50 +0400

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ st.markdown("""
 @st.cache_resource
 def load_model():
     try:
-       model = tf.keras.models.load_model('potato_disease_model.keras')
+        model = tf.keras.models.load_model('potato_disease_model.keras')
         return model
     except Exception as e:
         st.error(f"Մոդելը չբեռնվեց։ {e}")
